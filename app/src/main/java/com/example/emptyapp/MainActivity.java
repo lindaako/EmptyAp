5d21fb4f8815e8709f1100fdebf5fec499473f4e
linda <danielleleeveisella@gmail.com>
2019-05-07 00:00:23 +0900

    working code

--- app/src/main/java/com/example/emptyapp/MainActivity.java
+++ app/src/main/java/com/example/emptyapp/MainActivity.java
@@ -4,9 +4,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.io.IOException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -24,19 +40,30 @@ public class MainActivity extends AppCompatActivity
         mButton = findViewById(R.id.button);
         mEdit   = findViewById(R.id.edittext);
 
+
         mButton.setOnClickListener
                 (
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
-                        String query = mEdit.getText().toString();
-                        String url = "https://pyxis.knu.ac.kr/en/#/search/ex?all=1%7Ck%7Ca%7C" + query;
-                        Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(url));
-                        startActivity(i);
-                    }
-                });
+                     new View.OnClickListener()
+                        {
+                            public void onClick(View view)
+                            {
+
+                                String query = mEdit.getText().toString();
+                                String url = "https://pyxis.knu.ac.kr/en/#/search/ex?all=1%7Ck%7Ca%7C" + query + "&rq=BRANCH%3D1";
+
+
+                                Intent intent = new Intent(getApplicationContext(), ShowWebPage.class);
+                                intent.putExtra("url", url);
+                                startActivity(intent);
+
+                                //Intent i = new Intent(Intent.ACTION_VIEW);
+                                //i.setData(Uri.parse(url));
+                                //startActivity(i);
+                            }
+                        });
 
     }
+
+
 }
+
---
+++ app/src/main/java/com/example/emptyapp/ShowWebPage.java
@@ -0,0 +1,143 @@
+package com.example.emptyapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ShowWebPage extends AppCompatActivity
+{
+    WebView simpleWebView;
+    String url;
+    Document document;
+    Toast myBook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_web_page);
+
+        simpleWebView = findViewById(R.id.simpleWebView);
+        simpleWebView.setWebViewClient(new MyWebViewClient());
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null)
+        {
+            url = bundle.getString("url");
+        }
+
+        else
+        {
+            Toast errorToast = Toast.makeText(ShowWebPage.this, "Error sending data!", Toast.LENGTH_SHORT);
+            errorToast.show();
+        }
+
+        simpleWebView.getSettings().setJavaScriptEnabled(true);
+        simpleWebView.loadUrl(url); // load a web page in a web view
+
+
+
+    }
+
+    private class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon)
+        {
+            super.onPageStarted(view, url, favicon);
+
+            Log.d("WebView", "your current url when webpage loading.." + url);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url)
+        {
+            Log.d("WebView", "your current url when webpage loading.. finish " + url);
+
+            if ( url.contains("https://pyxis.knu.ac.kr/en/#/search/detail/") )
+            {
+                Toast myToast = Toast.makeText(ShowWebPage.this, url, Toast.LENGTH_SHORT);
+                myToast.show();
+/*
+                try
+                {
+                    document = Jsoup.connect(url).get();
+
+                    Element BookAvailabilityInfo = document.select("td").first();
+                    String htmlString = BookAvailabilityInfo.toString();
+
+                    if (htmlString.contains("Not Available") )
+                    {
+                        myBook = Toast.makeText(ShowWebPage.this, "Book is not Available", Toast.LENGTH_SHORT);
+                        myBook.show();
+                    }
+                    else
+                    {
+                        myBook = Toast.makeText(ShowWebPage.this, "Book is Available", Toast.LENGTH_SHORT);
+                        myBook.show();
+                    }
+
+
+                    for (Element urls : BookAvailabilityInfo)
+                    {
+                        if (((urls.text()).toString()).contains("Not Available") )
+                        {
+                            myBook = Toast.makeText(ShowWebPage.this, "Book is not Available", Toast.LENGTH_SHORT);
+                            myBook.show();
+                        }
+                        else
+                        {
+                            myBook = Toast.makeText(ShowWebPage.this, "Book is Available", Toast.LENGTH_SHORT);
+                            myBook.show();
+                        }
+
+                    }
+
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    document = null;
+                }*/
+            }
+
+
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onLoadResource(WebView view, String url)
+        {
+            // TODO Auto-generated method stub
+            super.onLoadResource(view, url);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            System.out.println("when you click on any interlink on webview that time you got url :-" + url);
+
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+       /*
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+
+            return true;
+        }*/
+    }
+}
