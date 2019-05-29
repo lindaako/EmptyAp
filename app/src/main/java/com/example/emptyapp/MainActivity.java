ca6f42b0c8c7824d6be74a44e1e128d2d63ccd2f
Linda <danielleleeveisella"gmail.com>
2019-05-29 16:59:51 +0900

    problem half fixed

--- app/src/main/java/com/example/emptyapp/MainActivity.java
+++ app/src/main/java/com/example/emptyapp/MainActivity.java
@@ -1,42 +1,68 @@
 package com.example.emptyapp;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.io.File;
 import java.io.IOException;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-//public class MainActivity extends AsyncTask<String, Void,>
+
 {
 
     Button mButton;
     EditText mEdit;
-    Toast myBook;
     public Document document = null;
+    String url_1 = "https://pyxis.knu.ac.kr/en/#/search/detail/";
+    private static final String FILE_NAME = "Detail_Kyungpook.html";
+    private static final int REQUEST_CODE = 2424;
 
+    private void verifyPermissions()
+    {
+        Log.d("This App", "verifyPermissions: Asking user fo permissions");
+        String[] permissions =
+                {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                };
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                        permissions[1]) == PackageManager.PERMISSION_GRANTED)
+        {
+
+        }
+
+        else
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,REQUEST_CODE);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        verifyPermissions();
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,64 +71,124 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
 
-
+        final WebView browser = findViewById(R.id.browser);
         mButton = findViewById(R.id.button);
         mEdit   = findViewById(R.id.edittext);
-
+        verifyPermissions();
 
         mButton.setOnClickListener
                 (new View.OnClickListener()
                         {
-                            public void onClick(View view)
-                            {
-                                final String url = "view-source:http://pyxis.knu.ac.kr/en/#/search/detail/4500295";
-/*
-                                try {
-                                    // Connect to the web site
-                                    Document mBlogDocument = Jsoup.connect(url).get();
-                                    // Using Elements to get the Meta data
-                                    Elements mElementDataSize = mBlogDocument.select("td");
-                                    // Locate the content attribute
-                                    int mElementSize = mElementDataSize.size();
 
-                                    for (int i = 0; i < mElementSize; i++)
-                                    {
-                                        Elements mElementAuthorName = mBlogDocument.select("span[class=ikc-item-status]").select("font").eq(i);
-                                        System.out.println(mElementAuthorName.text());
+                            @RequiresApi(api = Build.VERSION_CODES.N)
+                            public void onClick(View view) {
+                                /*
+                                String query = mEdit.getText().toString();
+                                String url = "https://pyxis.knu.ac.kr/en/#/search/ex?all=1%7Ck%7Ca%7C" + query + "&rq=BRANCH%3D1";
 
+                                Intent intent = new Intent(getApplicationContext(), ShowWebPage.class);
+                                intent.putExtra("url", url);
+                                startActivity(intent);
+                                */
+                                File My_File = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), FILE_NAME);//if ready then create a file for external
+                                /*
+                                File External_File = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Myfile.txt");
+                                String text = "Fizzle rocks";
+                                FileOutputStream fos = null;
 
-                                    }
+                                try
+                                {
+                                    fos = new FileOutputStream(External_File);
+                                    //fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+
+                                    fos.write(text.getBytes());
+
+                                    Toast.makeText(MainActivity.this, "Saved to " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+ "/" + FILE_NAME, Toast.LENGTH_LONG).show()
+                                    ;
+
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
                                 } catch (IOException e) {
                                     e.printStackTrace();
+                                } finally {
+                                    if (fos != null) {
+                                        try {
+                                            fos.close();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
                                 }
 
-                                */
+
+                                FileInputStream fis = null;
+
+                                try {
+
+                                    fis = new FileInputStream(External_File);
+                                    //DataInputStream in = new DataInputStream(fis);
+                                    //BufferedReader br =new BufferedReader(new InputStreamReader(in));
+                                    //String strLine;
+
+                                    //fis = openFileInput(FILE_NAME);
+                                    InputStreamReader isr = new InputStreamReader(fis);
+                                    BufferedReader br = new BufferedReader(isr);
+                                    StringBuilder sb = new StringBuilder();
+                                    String text2;
+
+                                    while ((text2 = br.readLine()) != null)
+                                    {
+                                        sb.append(text2).append("\n");
+                                    }
+                                    System.out.println(sb);
+
+                                    Toast.makeText(MainActivity.this, sb, Toast.LENGTH_LONG).show()
+                                    ;
+
+
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    if (fis != null) {
+                                        try {
+                                            fis.close();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+*/
 
                                 Thread thread = new Thread(new Runnable()
                                 {
                                     @Override
                                     public void run()
                                     {
+                                        /* An instance of this class will be registered as a JavaScript interface */
 
 
-                                        try
-                                        {
+                                        try {
                                             // Connect to the web site
-                                            Document mBlogDocument = Jsoup.connect("https://pyxis.knu.ac.kr/#/search/detail/4500295").get();
 
+                                            File input = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), FILE_NAME);
+                                            Document mBlogDocument = Jsoup.parse(input, "UTF-8", url_1);
+                                            //Document mBlogDocument = Jsoup.connect(url).get();
+                                            //System.out.println(mBlogDocument);
 
-                                            System.out.println(mBlogDocument);
                                             // Using Elements to get the Meta data
-                                            Elements mElementDataSize = mBlogDocument.select("div[class=ikc-pyxis-wrap]");
+                                            Elements mElementDataSize = mBlogDocument.select("span[class=ikc-item-status]");
                                             //System.out.println(mElementDataSize);
                                             // Locate the content attribute
                                             int mElementSize = mElementDataSize.size();
                                             System.out.println(mElementSize);
 
+
                                             for (int i = 0; i < mElementSize; i++)
                                             {
-                                                Elements mElementAuthorName = mBlogDocument.select("span[class=ikc-item-status]").select("font").eq(i);
-                                                System.out.println(mElementAuthorName.text());
+                                                Element BookStatusData = mBlogDocument.select("span.ikc-item-status").get(i);
+                                                System.out.println(BookStatusData.text());
 
 
                                             }
@@ -110,82 +196,6 @@ public class MainActivity extends AppCompatActivity
                                             e.printStackTrace();
                                         }
 
-
-                                        /*
-
-                                        try
-                                        {
-
-                                            Log.d("MainActivity", " trying... ");
-                                           document = Jsoup.connect(url)
-                                            //document = Jsoup.connect("http://example.com")
-                                                    .data("query","Java")
-                                                    .userAgent("Mozilla")
-                                                    .cookie("auth","token")
-                                                    .timeout(6000)
-                                                    .post();
-
-                                            Log.d("MainActivity", " Done! ");
-
-                                            if (document == null)
-                                            {
-                                                Log.d("MainActivity", "\n The Document is empty!\n");
-                                            }
-
-                                            if(document != null)
-                                            {
-                                                Elements spanTags = document.select("span.ikc-item-status");
-                                                for (Element spanTag : spanTags)
-                                                {
-                                                    String text = spanTag.ownText();
-                                                    System.out.println(text);
-                                                }
-                                                Log.d("MainActivity", " Done getting data! ");
-
-
-                                                Elements top = document.select("tbody");
-                                                Elements top_1 = top.select("tr");
-                                                Elements top_2 = top_1.select("td");
-                                                Elements top_3 = top_2.select("span");
-                                                //Elements options = document.getElementsByClass("font");
-                                                //Elements options = document.getElementsByTag("span.ikc-item-status");
-                                                 int i = 1;
-                                                for (Element element : top_3)
-                                                {
-                                                    //Log.d("MainActivity", "\nDone\n");
-                                                    //Log.d("MainActivity", element.text());
-                                                    System.out.println("string " + i + ": " + element.text());
-                                                    i++;
-                                                }
-
-                                                Elements myElements = document.getElementsByTag("td")
-                                                        .first().getElementsByTag("span");
-                                                for (Element element : myElements)
-                                                {
-                                                    if (element.className().contains("ikc-item-status"))
-                                                    {
-                                                        String Content = element.ownText();
-
-
-                                                        System.out.println("   " + Content + " " );
-                                                    }
-                                                }
-
-
-                                            }
-
-
-                                        }
-                                        catch (IOException e)
-                                        {
-                                            Log.d("MainActivity", "ERROR!\n");
-                                            e.printStackTrace();
-                                            document = null;
-
-
-                                        }
-
-*/
                                     }
 
 
@@ -207,33 +217,6 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+
+
 }
-
-/*
-                                if (document != null)
-                                {
-                                    Elements options = document.select("td>span.ikc-item-status");
-                                    int i = 1;
-                                    for (Element element : options)
-                                    {
-                                        //System.out.println("string"+i+element.text());
-                                        myBook = Toast.makeText(MainActivity.this, element.text(), Toast.LENGTH_SHORT);
-                                        myBook.show();
-                                        i++;
-                                    }
-                                }
-                                else if (document == null)
-                                {
-                                    myBook = Toast.makeText(MainActivity.this, "empty documnet error", Toast.LENGTH_SHORT);
-                                    myBook.show();
-                                }
-
-
-                                String query = mEdit.getText().toString();
-                                String url = "https://pyxis.knu.ac.kr/en/#/search/ex?all=1%7Ck%7Ca%7C" + query + "&rq=BRANCH%3D1";
-
-
-                                Intent intent = new Intent(getApplicationContext(), ShowWebPage.class);
-                                intent.putExtra("url", url);
-                                startActivity(intent);
-                                */
--- app/src/main/java/com/example/emptyapp/ShowWebPage.java
+++ app/src/main/java/com/example/emptyapp/ShowWebPage.java
@@ -1,6 +1,5 @@
 package com.example.emptyapp;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.widget.Toast;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -29,7 +27,7 @@ public class ShowWebPage extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_web_page);
 
-        simpleWebView = findViewById(R.id.simpleWebView);
+        simpleWebView = findViewById(R.id.browser);
         simpleWebView.setWebViewClient(new MyWebViewClient());
 
         Bundle bundle = getIntent().getExtras();
